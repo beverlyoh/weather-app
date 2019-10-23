8ff3711a33314cb1aa22dad460f490b4c3f7f1f7
Beverly Oh <beverlyoh@gmail.com>
2019-10-23 20:01:28 +0800

    Updating App.js to render weather and feedback form

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import CurrentWeather from './CurrentWeather'; 
+import ComplaintPage from './pages/ComplaintPage';
+import WeatherPage from './pages/WeatherPage';
 
 function App() {
   return (
-    <div className="App">
-      <CurrentWeather country="UK" city="London" temperature="13"/>
-      <CurrentWeather country="Australia" city="Perth" temperature="21"/>
-      <CurrentWeather country="Singapore" city="Singapore" temperature="31"/>
-    </div>
+    <>
+      <WeatherPage />
+      <ComplaintPage />
+    </>
   );
 }
 
